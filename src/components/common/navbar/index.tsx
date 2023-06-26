3177851b9ec928ef2a71efc7d6669ad0f751cba1
Muhammad Rizky Sya'ban <mrsyaban99@gmail.com>
2023-06-26 11:47:20 +0800

    feat: add navbar

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -1,10 +1,28 @@
-import Search from './assets/react.svg';
+import Logo from './../../../assets/logo.svg';
+
+import Search from './../../../assets/search_icon.svg';
+import Cart from './../../../assets/cart_icon.svg';
+import Favorite from './../../../assets/love_icon.svg';
+import Profile from './../../../assets/user_icon.svg';
 
 
 const Navbar = () => {
   return (
-    <div >
-
+    <div className='flex h-20 items-center px-32 justify-between border border-[#929191]'>
+      <div className='flex h-20 items-center space-x-12 font-bold'>
+        <img src={Logo} alt='' className='h-32'/>
+        <a>Pria</a>
+        <a>Wanita</a>
+        <a>Brands</a>
+      </div>
+      <div className='flex h-20 items-center space-x-12 font-bold'>
+        <img src={Search} alt='' className='w-7'/>
+        <img src={Cart} alt='' className='w-7'/>
+        <img src={Favorite} alt='' className='w-7'/>
+        <img src={Profile} alt='' className='w-7'/>
+      </div>
     </div>
   );
 }
+
+export default Navbar;
