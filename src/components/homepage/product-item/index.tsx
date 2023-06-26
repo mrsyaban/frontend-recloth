16b81b857993e92cf5dd1093f8c0c4c945757b17
Muhammad Rizky Sya'ban <mrsyaban99@gmail.com>
2023-06-26 12:04:25 +0800

    feat: edit productitem at homepage

--- src/components/homepage/product-item/index.tsx
+++ src/components/homepage/product-item/index.tsx
@@ -4,12 +4,15 @@ const ProductItem = () => {
   return (
     <div className="flex flex-col bg-cream drop-shadow-md">
       <img src={ImageProduct} alt="" className='w-full'/>
-      <div className="flex px-8">
+      <div className="flex px-6 py-2 justify-between">
         <div className="flex flex-col">
+          Rp 35.000
+          <br/>
           XL
         </div>
-        <div className="flex flex-col">
-
+        <div className="flex flex-col text-xs">
+          Donate
+          <div className='text-xl'>70%</div>
         </div>
       </div>
     </div>
