563de738342b4278e6956cc6fa3f5341fa37dc7f
Muhammad Rizky Sya'ban <mrsyaban99@gmail.com>
2023-06-26 12:46:09 +0800

    feat: add product page

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,7 +1,29 @@
+import ImageProduct from './../../assets/images/product_image.png'
+
 const ProductPage = () => {
   return (
-    <>
-    </>
+    <div className='px-32'>
+      <img src={ImageProduct} alt='product image' className='w-2/6'/>
+      <div className='flex flex-col w-2/6'>
+        <h1> Jaket Ngab </h1>
+        <h2> Rp 35.000</h2>
+        <p>
+          XL
+          <br/>
+          Panjang : 58cm
+          <br/>
+          Lebar : 58cm
+        </p>
+        <div>
+          <button className='bg-cream'>
+            TAMBAHKAN KE KERANJANG
+          </button>
+          <button className='bg-grey'>
+            DONASI
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
