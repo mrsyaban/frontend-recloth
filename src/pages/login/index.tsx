03041d4cb8007d432347b7ad98fef6eceb3030ab
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2023-07-11 21:37:54 +0700

    feat: update some parts on login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,9 +7,16 @@ const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const printHeaders = (headers) => {
+    for (const [key, value] of Object.entries(headers)) {
+      console.log(`${key}: ${value}`);
+    }
+  };
+  
   const handleLogin = async() => {
     try {
       console.log("eee");
+      // axios.defaults.withCredentials=true;
       const response = await axios.post(
         "https://reclothserver.azurewebsites.net/auth/login",
         {
@@ -19,6 +26,8 @@ const LoginPage = () => {
       )
       
       console.log(response.data);
+      console.log(response.headers);
+      printHeaders(response.headers);
       // Mengakses cookie dari respons
       const cookie = await response.headers['Set-Cookie'];
       console.log(cookie);
@@ -26,15 +35,18 @@ const LoginPage = () => {
       console.error(error);
     }
   }
+
   return (
     <div className="flex flex-col bg-grey h-screen">
       <div className="flex justify-end mr-16 mt-10 space-x-5">
-        <Link className="text-black self-center round" to="/">
+      <Link className="text-black self-center round" to="/">
           HOME
         </Link>
-        <button className="bg-cream rounded p-3 text-black font-bold">
-          SIGN UP
-        </button>
+        <Link className="text-black self-center round" to="/registry">
+          <button className="bg-cream rounded p-3 text-black font-bold">
+            SIGN UP
+          </button>
+        </Link>
       </div>
       <div className="flex flex-col bg-cream w-[60vh] my-auto mx-auto p-10 rounded-md space-y-10">
         <h1 className="flex font-bold text-black text-3xl self-center">
@@ -46,12 +58,12 @@ const LoginPage = () => {
           password={password}
           setPassword={setPassword}
         />
-        <div 
+        <button 
           className="flex bg-black rounded justify-center py-2 text-cream font-bold"
           onClick={handleLogin}
         >
           L O G I N
-        </div>
+        </button>
       </div>
     </div>
   );
